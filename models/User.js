10b1a6e4d1a7082c2cab028dad31c6f7737dde43
young chhay <ungyoungchhay@gmail.com>
2022-10-15 14:41:13 -0700

    edit and connect Signup/Login js with handlebars

--- models/User.js
+++ models/User.js
@@ -35,25 +35,25 @@ User.init(
         len: [8],
       },
     },
-    // I was not sure if carown has to be boolean data type? (Richard)
-    carown: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [8],
-        },
-      },
-      // I was not sure this if its goingto be string? if its year, maybe it should be Number? 
-    carmadeyear: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [4],
-        },
-      },
+    // // I was not sure if carown has to be boolean data type? (Richard)
+    // carown: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    //     validate: {
+    //       len: [8],
+    //     },
+    //   },
+    //   // I was not sure this if its goingto be string? if its year, maybe it should be Number? 
+    // carmadeyear: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false,
+    //     validate: {
+    //       len: [4],
+    //     },
+    //   },
         // and id should be Number as well? 
     car_id: {
-        type: DataTypes.STRING,
+        type: DataTypes.INTEGER,
         allowNull: false,
         references: {
                 model: 'Car',
--- public/js/login.js
+++ public/js/login.js
@@ -1,55 +1,55 @@
 // handle login form
 const loginFormHandler = async (event) => {
-    // Stop the browser from conituosly submitting the form so we can do so with JavaScript
-    event.preventDefault();
-  
-    // Gather the data from the form elements on the page
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
-      // Send the e-mail and password to the server
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
+  // Stop the browser from conituosly submitting the form so we can do so with JavaScript
+  event.preventDefault();
+
+  // Gather the data from the form elements on the page
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    // Send the e-mail and password to the server
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
-  };
+  }
+};
 
 // handle Signup form
-  const signUpFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#signup-name').value.trim();
-    const email = document.querySelector('#signup-email').value.trim();
-    const password = document.querySelector('#signup-password').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+const signUpFormHandler = async (event) => {
+  event.preventDefault();
 
-    document
+  const name = document.querySelector('#signup-name').value.trim();
+  const email = document.querySelector('#signup-email').value.trim();
+  const password = document.querySelector('#signup-password').value.trim();
+  const confirmPassword = document.querySelector('#confirm-password').value.trim();
+  if (name && email && (password === confirmPassword)) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.login100-form')
+  .addEventListener('submit', loginFormHandler);
+
+document
   .querySelector('.signup-form')
-  .addEventListener('submit', signupFormHandler);
+  .addEventListener('click', signUpFormHandler);
