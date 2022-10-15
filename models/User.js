42a492afdf98c3bfd7640a1459f11be1466d185f
Nunder <nathannunderwood@gmail.com>
2022-10-14 21:12:04 -0700

    uncomment models so index can call on data. comment out models not being used yet, like 'project'

--- models/User.js
+++ models/User.js
@@ -1,68 +1,68 @@
-// const { Model, DataTypes } = require('sequelize');
-// const bcrypt = require('bcrypt');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-// class User extends Model {
-//   checkPassword(loginPw) {
-//     return bcrypt.compareSync(loginPw, this.password);
-//   }
-// }
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-// User.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     name: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     email: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//       validate: {
-//         isEmail: true,
-//       },
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       validate: {
-//         len: [8],
-//       },
-//     },
-//     carown: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         validate: {
-//           len: [8],
-//         },
-//       },
-//     carmadeyear: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         validate: {
-//           len: [4],
-//         },
-//       },
-//   },
-//   {
-//     hooks: {
-//       beforeCreate: async (newUserData) => {
-//         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-//         return newUserData;
-//       },
-//     },
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'user',
-//   }
-// );
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+    carown: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [8],
+        },
+      },
+    carmadeyear: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4],
+        },
+      },
+  },
+  {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
 
-// module.exports = User;
+module.exports = User;
--- models/car.js
+++ models/car.js
@@ -1,43 +1,43 @@
-// const { Model, DataTypes } = require('sequelize');
-// const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-// class Car extends Model {}
+class Car extends Model {}
 
-// Car.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     year: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//     },
-//     make: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     model: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     value: {
-//         type: DataTypes.INTEGER, 
-//         allowNull: false,
-//     }, 
-//     trade: {
-//         type: DataTypes.BOOLEAN, 
-//         allowNull: false,
-//     },
-//   },
-//   {
-//     sequelize,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'car',
-//   }
-// );
+Car.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    value: {
+        type: DataTypes.INTEGER, 
+        allowNull: false,
+    }, 
+    trade: {
+        type: DataTypes.BOOLEAN, 
+        allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'car',
+  }
+);
 
-// module.exports = Car;
+module.exports = Car;
