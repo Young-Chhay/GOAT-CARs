281eec9f8fd8063eb27e738ccd112bc31c06f426
RichardBass You <richardbassyou@RichardBasssMBP.lan>
2022-10-16 23:10:55 -0700

    merge main for Drum's commit

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,12 +3,14 @@ const carRoutes = require('./car-routes');
 const userRoutes = require('./user-routes');
 const forumRoutes = require('./forum-routes');
 const auctionRoutes = require('./auction-routes');
+const merchandiseRoutes = require('./merchandise-routes')
 
 
 // End Points
 // router.use('/cars', carRoutes);
 router.use('/users', userRoutes);
 router.use('/forum', forumRoutes);
+router.use('/merchandise', merchandiseRoutes)
 // router.use('/auctions', auctionRoutes);
 // router.use('/', homeRoutes);
 
--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+
+
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ 
+  // helpers
+});
+
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -23,9 +33,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({ 
-  helpers
-});
+
 
 // hbs.getPartials().then(function (partials) {
 //   console.log(partials);
@@ -39,8 +47,9 @@ app.use("/", express.static(path.join(__dirname, 'public')));
 app.use("/forum", express.static(path.join(__dirname, 'public')));
 app.use("/merchandise", express.static(path.join(__dirname, 'public')));
 
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening at localhost:3001'));
+  app.listen(PORT, () => console.log('Now listening'));
 });
