edb4662ad73cf96c7cbb67c25c23e8fb08a153b4
RichardBass You <richardbassyou@RichardBasssMBP.lan>
2022-10-17 19:36:27 -0700

    merge from Young's merge

--- controllers/forumRoutes.js
+++ controllers/forumRoutes.js
@@ -2,13 +2,25 @@ const router = require('express').Router();
 const Forum = require('../models/Forum');
 const User = require('../models/User');
 const withAuth = require('../utils/auth');
+const { QueryTypes } = require('sequelize');
 
 router.get('/', async (req,res) => {
     try {
+        const query =  "SELECT * FROM forum ORDER BY data_created DESC LIMIT 3";
+        const [results, metadata] = await sequelize.query(query)
+        // const forumData = await sequelize.query(query,
+        //     {
+        //         bind: ['active'],
+        //         type: QueryTypes.SELECT
+        //     }
+        //     ); 
+        const forumData = results;
+            const forums = forumData.map((forum) => forum.get({ plain: true }))
         res.render('forum-main', {
+            forums,
             logged_in: req.session.logged_in
         }, 
-        );
+    );
     } catch (err) {
         res.status(500).json(err);
     }
