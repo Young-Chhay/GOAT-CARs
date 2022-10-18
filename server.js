4891c3be10da89bc5aeaaee430b1b733fc52ee5b
Young C <skykeller80@gmail.com>
2022-10-17 22:53:52 -0700

    added Cars data for gallery, edit gallery handlebar,

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname, 'public')));
 app.use("/forum", express.static(path.join(__dirname, 'public')));
 app.use("/merchandise", express.static(path.join(__dirname, 'public')));
-
+app.use("/gallery", express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
