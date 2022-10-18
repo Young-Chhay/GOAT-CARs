b8244d15c3242a0e1e08508a6d0a39eddee45e0b
Young C <skykeller80@gmail.com>
2022-10-17 21:11:37 -0700

    added galleryRoutes, gallery handlebar, need further review

--- models/car.js
+++ models/car.js
@@ -1,45 +1,45 @@
-// const { Model, DataTypes } = require('sequelize');
-// // connection.js to connect Node to MySQL
-// const sequelize = require('../config/connection.js');
+const { Model, DataTypes } = require('sequelize');
+// connection.js to connect Node to MySQL
+const sequelize = require('../config/connection.js');
 
-// class Car extends Model { }
+class Car extends Model { }
 
-// Car.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     year: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     make: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     model: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     value: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     // NOt using trade
-//     // trade: {
-//     //   type: DataTypes.BOOLEAN,
-//     //   allowNull: false,
-//     // },
-//   },
-//   {
-//     sequelize,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'car',
-//   }
-// );
+Car.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    make: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    model: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    // NOt using trade
+    // trade: {
+    //   type: DataTypes.BOOLEAN,
+    //   allowNull: false,
+    // },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'car',
+  }
+);
 
-// module.exports = Car;
+module.exports = Car;
