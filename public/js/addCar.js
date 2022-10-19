e0031468deaf0514f6796aa7966e0c5be80ddb04
RichardBass You <richardbassyou@RichardBasss-MacBook-Pro.local>
2022-10-18 20:41:01 -0700

    fixing carModel

--- public/js/addCar.js
+++ public/js/addCar.js
@@ -9,15 +9,16 @@ const addCarHandler = async (event) => {
     const model = document.querySelector('#car-model').value.trim();
     const value = document.querySelector('#car-value').value.trim();
     const image = document.querySelector('#car-image').value.trim();
+    const description = document.querySelector('#car-description').value.trim();
 
     // console.log(year);
     // console.log(make);
     // console.log(model);
     // console.log(image);
-    if (year && make && model && value && image) {
+    if (year && make && model && value && image && description) {
         const response = await fetch('/api/car', {
             method: 'POST',
-            body: JSON.stringify({ year, make, model, value, image }),
+            body: JSON.stringify({ year, make, model, value, image, description }),
             headers: { 'Content-Type': 'application/json' },
         });
 
